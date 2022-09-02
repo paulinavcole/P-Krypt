833947b61be373aa901b85ea25e4de4dc82d667a
Paulina Cole <lina.cole@gmail.com>
2022-09-02 17:32:56 -0400

    address from showing for every transaction from goerli etherscan

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -3,6 +3,29 @@ import React, { useContext } from 'react';
 
 import { TransactionContext } from '../context/TransactionContext'
 
+import { shortenAddress } from '../utils/shortenAddress';
+import dummyData from "../utils/dummyData";
+
+const TransactionCard = ( {addressTo, addressFrom, timestamp, message, keyword, amount, url }) => {
+    return (
+        <div className='bg-[#181918] m-4 flex flex-1
+            2xl:min-w-[450px]
+            2xl:max-w-[500px]
+            sm:min-w-[270px]
+            sm:max-w-[300px]
+            flex-col p-3 rounded-md hover:shadow-2xl
+        '>
+            <div className='display-flex justify-start w-full mb-6 p-2"'>
+                <div className='flex justify-start w-full mb-6 p-2'>
+                    <a href={`https://goerli.etherscan.io/address/${addressFrom}`} target="_blank" rel="noreferrer">
+                        <p className="text-white text-base">From: {shortenAddress(addressFrom)}</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 const Transactions = () => {
     const { currentAccount } = useContext(TransactionContext);
 
@@ -14,6 +37,11 @@ const Transactions = () => {
                 ): ( 
                     <h3 className='text-white text-3xl text-center my-2'>Connect your account to see your Transactions</h3>
                 )}
+                <div className='flex flex-wrap justify-center items-center mt-10'>
+                    {dummyData.reverse().map((transaction, i) => (
+                        <TransactionCard  key={i} {...transaction}/>
+                    ))}
+                </div>
             </div>
         </div>
     );
