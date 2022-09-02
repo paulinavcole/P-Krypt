e82f772b19eb6b20b721b5f262ad2ad3d18f86d8
Paulina Cole <lina.cole@gmail.com>
2022-09-02 10:54:10 -0400

    creating transaction context including ethereum contract

---
+++ client/src/context/TransactionContext.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+
+import { contractABI, contractAddress } from '../utils/constants';
+
+export const TransactionContext = react.createContext();
+
+const { ethereum } = window
+
+const getEthereumContract = () => {
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const transactionContract = new ethers.Contract(contractAddress, contractABI, signer)
+
+    console.log({
+        provider, 
+        signer, 
+        transactionContract
+    })
+}
