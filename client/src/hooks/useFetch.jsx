79458e21d42c4197841bb4f19b211c5f5022949f
Paulina Cole <lina.cole@gmail.com>
2022-09-02 19:02:23 -0400

    gifs rendering

--- client/src/hooks/useFetch.jsx
+++ client/src/hooks/useFetch.jsx
@@ -13,11 +13,15 @@ const useFetch = ({ keyword }) => {
             setGifUrl(data[0]?.images?.downsized_medium?.url);
 
         } catch(ex) {
-            setGifUrl('https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284')
+            setGifUrl('https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284');
         }
     }
 
     useEffect(() => {
+        if(keyword) fetchGifs();
+    }, [keyword]);
 
-    }, [keyword])
+    return gifUrl;
 }
+
+export default useFetch;
