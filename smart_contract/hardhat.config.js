78c1cf7f61516a94bc5bc94b1c134428271d68f7
Paulina Cole <lina.cole@gmail.com>
2022-09-02 10:17:00 -0400

    updated hard hat config file

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+//https://eth-goerli.g.alchemy.com/v2/3E4cLBPFhVx0NPKbwwWRUX2Bu838307R
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+
 module.exports = {
-  solidity: "0.8.9",
-};
+  solidity: '0.8.0',
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/3E4cLBPFhVx0NPKbwwWRUX2Bu838307R',
+      accounts: [ '56fefcc6ff18721ff616f232ed77251d9f1b5053437eb3eea53bff803673c31b' ]
+    }
+  }
+}
