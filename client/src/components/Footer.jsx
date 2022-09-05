4400c2ba00c43713fa72b83f5f40de5c35ad01cd
Paulina Cole <lina.cole@gmail.com>
2022-09-05 11:44:10 -0400

    updating pre deployment

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -10,6 +10,8 @@ const Footer = () => {
                         alt='logo'
                         className='w-40'
                     />
+                    <p className='text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base'>Send and Receive Crypto for All ©</p>
+                    <p className='text-white justify-between items-center my-4'>For more info, contact us at info@kryptotoolkit.com</p>
                 </div>
             </div>
         </div>
